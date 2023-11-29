3d479d13af851dc8aadc68a71911c524ba4552de
mosesboriowo <boriowomoses2@gmail.com>
2023-11-29 17:23:28 +0100

    add instance connect

--- main.tf
+++ main.tf
@@ -167,13 +167,12 @@ resource "aws_security_group" "ec2-sec" {
   description = "created using terraform"
   vpc_id      = "${aws_vpc.vpc.id}"
 
+  // Inbound rule allowing SSH (port 22) access from anywhere (0.0.0.0/0)
   ingress {
-    description      = "All traffic"
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Restrict this to your IP or specific IPs if possible
   }
   
 

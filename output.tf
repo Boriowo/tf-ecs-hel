f643a1212703146b6a414c26f6c671d97c869d76
amakshata <131159907+amakshata@users.noreply.github.com>
2023-11-29 19:26:33 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,14 @@ output "InstanceId" {
   description = "ID of the EC2 instance"
   value       = aws_instance.ec2-instance.id
 }
+
+output "private_key_pem" {
+  description = "The private key data in PEM format"
+  value       = tls_private_key.ec2-key-pair.private_key_pem
+  sensitive   = true
+}
+ 
+output "public_key_openssh" {
+  description = "The public key data in OpenSSH authorized_keys format"
+  value       = tls_private_key.ec2-public-key.public_key_openssh
+}
